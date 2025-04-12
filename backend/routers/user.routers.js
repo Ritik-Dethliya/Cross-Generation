e667ce159277ac660be8e7234bc972b6d308862d
Ritik-Dethliya <dethliya73@gmail.com>
2025-04-12 18:00:31 +0530

    pu

--- backend/routers/user.routers.js
+++ backend/routers/user.routers.js
@@ -14,7 +14,7 @@ userrouter.post('/create-user/', createUser);
 userrouter.get('/get-user/', getUsers);
 userrouter.get('/get-user/:id', getUserById);
 userrouter.put('/update-user/:id', updateUser);
-userrouter.get('/get-mentors',getMentors)
+userrouter.get('/get-mentors/',getMentors)
 userrouter.delete('/delete-user/:id', deleteUser);
 
 export default userrouter;
