2196d028e9d8f26c9c2a1039a3c1f8d8692a851c
Ritik-Dethliya <dethliya73@gmail.com>
2025-04-12 09:08:20 +0530

    create mongo altas

--- backend/config.db.js
+++ backend/config.db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDb=async()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/Anti-resume")
+    mongoose.connect(process.env.MONGO_URI)
     .then(console.log("connect to bd"))
     .catch((error)=>{
-        console.log("error in connection")
+        console.log("error in connection",error)
     })
 }
 export default connectDb
